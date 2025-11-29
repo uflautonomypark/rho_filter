de7f8cc807088df639050b9557c267c86602e498
Max Gardenswartz <max.gardenswartz@proton.me>
2025-11-29 17:03:57 -0500

    Something broke the web app

---
+++ serve.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+import mimetypes
+import os
+
+# Force correct Wasm MIME type
+mimetypes.init()
+mimetypes.add_type('application/wasm', '.wasm')
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="public", **kwargs)
+
+print("Serving on http://localhost:8000")
+with socketserver.TCPServer(("", 8000), Handler) as httpd:
+    httpd.serve_forever()
